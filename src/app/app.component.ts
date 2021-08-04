cfc631a44bc27e193704c92830145b1df75db3c8
DeivisMaziukas <deivismaziuk@gmail.com>
2021-08-04 17:56:50 +0300

    First commit
    First ever commit - lots of work to do!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, AfterViewInit, ViewChildren, HostListener  } from '@angular/core';
+import { Updater } from 'tsparticles/Core/Particle/Updater';
+import { drawConnectLine } from 'tsparticles/Utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'personal-site';
+export class AppComponent implements AfterViewInit {
+
+
+  // its important myCanvas matches the variable name in the template
+  //@ViewChild('myCanvas', {static: true}) myCanvas: ElementRef;
+  //private ctx: CanvasRenderingContext2D;
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+
+    this.w = event.target.innerWidth;
+    this.h = event.target.innerHeight;
+    console.log(this.h);
+    console.log(this.w);
+  }
+
+  private w: number = window.innerWidth;
+  private h: number = window.innerHeight;
+
+  ngAfterViewInit(): void {
+    //this.drawBackground();
+   // window.requestAnimationFrame(this.random(2,4));
+  }
+/*
+  drawBackground() {
+    this.ctx = this.myCanvas.nativeElement.getContext('2d');
+    this.ctx.fillStyle = "#000000";
+    this.ctx.fillRect(0, 0, this.myCanvas.nativeElement.width, this.myCanvas.nativeElement.height);
+  }
+*/
+/*
+  random(min, max) {
+    console.log('test');
+    return Math.random() * (max - min) + min;
+  }
+*/
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterComponent } from './scatter/scatter.component';
+import {NgParticlesModule} from "ng-particles";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarComponent,
+    PieComponent,
+    ScatterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
